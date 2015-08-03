12a8f30964e522a2c760445929cea7f0ba47e9e1
Sascha Grossenbacher <saschagros@gmail.com>
2015-08-03 16:18:17 +0200

    More debug output for removed routes

--- src/Routing/PageManagerRoutes.php
+++ src/Routing/PageManagerRoutes.php
@@ -76,7 +76,11 @@ class PageManagerRoutes extends RouteSubscriberBase {
 
         if ($path == $route_path) {
 
-          $event->addArgument('remove_' . $route_name, $route_path);
+          $event->addArgument('remove_' . $route_name, [
+            'route_path' => $route_path,
+            'collection_route_name' => $name,
+            'collection_route' => print_r($collection_route, TRUE),
+          ]);
 
           // Adjust the path to translate %placeholders to {slugs}.
           $path = $collection_route->getPath();
